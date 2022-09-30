daab0e67d170a435f094980b824395f6e838d331
IlMike22 <michaelwidlok@yahoo.de>
2022-09-30 14:23:24 +0200

    add base adapter

--- app/src/main/java/de/mindmarket/spotifyclone/adapters/SwipeSongAdapter.kt
+++ app/src/main/java/de/mindmarket/spotifyclone/adapters/SwipeSongAdapter.kt
@@ -1,28 +1,18 @@
 package de.mindmarket.spotifyclone.adapters
 
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.recyclerview.widget.AsyncListDiffer
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.RequestManager
 import com.plcoding.spotifycloneyt.R
 import de.mindmarket.spotifyclone.remote.dto.Song
-import kotlinx.android.synthetic.main.list_item.view.*
-import javax.inject.Inject
+import kotlinx.android.synthetic.main.swipe_item.view.*
 
-class SongAdapter @Inject constructor(
-    private val glide: RequestManager,
-) : BaseSongAdapter(R.layout.list_item) {
+class SwipeSongAdapter() : BaseSongAdapter(R.layout.swipe_item) {
     override val differ: AsyncListDiffer<Song> = AsyncListDiffer(this, diffCallback)
 
     override fun onBindViewHolder(holder: SongViewHolder, position: Int) {
         val currentSong = songs[position]
         holder.itemView.apply {
-            tvPrimary.text = currentSong.title
-            tvSecondary.text = currentSong.subtitle
-            glide.load(currentSong.imageUrl).into(ivItemImage)
+            val text = "${currentSong.title} - ${currentSong.subtitle}"
+            tvPrimary.text = text
             setOnClickListener {
                 onItemClickListener?.let { onClicked ->
                     onClicked(currentSong)
